fef1033ce75544853ff6b5bae7e5d23afcec9114
rezaafsharniakan <reza.afsharniakan@gmail.com>
2021-07-19 21:32:28 +0430

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path, include
+
+from . import views
+
+app_name='blog'
+urlpatterns = [
+
+    path('', views.all_blog, name='all_blog'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+
+
+]
